b2e7a8869718657680d60adf69ab6a516ea7f013
vivekmaurya <maurya9695v@gmail.com>
2026-06-01 12:37:19 +0530

    Add files via upload

---
+++ TowerOfHanoi.java
@@ -0,0 +1,17 @@
+class TOwerOfHanoi {
+    public static void TOH(int n,char A,char B,char C) {
+        if(n==1){
+            System.out.println("Move 1 from " + A + " to " + C);
+            return;
+        }
+        TOH(n-1,A,C,B);
+        System.out.println("Move " + n + " from " + A + " to " + C);
+        TOH(n-1,B,A,C);
+        
+    }
+    public static void main(String[] args) {
+        int n=3;
+        TOH(n,'A','B','C');
+    }
+    
+}
