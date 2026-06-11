b330734925a3acfbdcbc6d42c3dd10016440041e
vivekmaurya <maurya9695v@gmail.com>
2026-06-11 22:03:18 +0530

    Refactor LargestElement class for efficiency

--- LargestElement.java
+++ LargestElement.java
@@ -41,7 +41,7 @@ class LargestElement {
 class LargestElement{
     public static int largestElement(int arr[]){
         int max=0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if(arr[i] > max){
                 max = arr[i];
             }
