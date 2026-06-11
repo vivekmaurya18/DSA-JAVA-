1c01e49120cf6827f12b8a3e4fc66c95a56e9f6b
vivekmaurya <maurya9695v@gmail.com>
2026-06-11 21:59:37 +0530

    Add files via upload

---
+++ LargestElement.java
@@ -0,0 +1,55 @@
+
+/*------------------------Naive Approach------------------------------------ */
+
+/* 
+class LargestElement {
+
+    public static int largestElement(int arr[]) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag == true) {
+                return i;   // index of largest element
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 20, 10};
+
+        System.out.println(largestElement(arr));
+    }
+}
+    
+*/
+
+/*---------------------------------Efficient Approach---------------------------------- */
+
+
+class LargestElement{
+    public static int largestElement(int arr[]){
+        int max=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr={5,8,150,20,103};
+        System.out.println(largestElement(arr));
+    }
+}
